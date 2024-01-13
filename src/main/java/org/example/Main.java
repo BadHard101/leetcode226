36d508b6915b686f5c446fb2397faaaf75bcf9b7
BadHard <BadHard101@yandex.ru>
2024-01-13 16:51:01 +0300

    initial

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,16 @@
 package org.example;
 
 public class Main {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
+
+        if (root.right != null) invertTree(root.right);
+        if (root.left != null) invertTree(root.left);
+        return root;
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
